9dbdbf184510de18b85ad47d14bf846732dffe2e
Fabio Tedesco <fabio.tedesco1997@gmail.com>
2025-01-16 19:05:02 +0100

    deploy test n.2

--- BE/core/db.php
+++ BE/core/db.php
@@ -8,10 +8,10 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-define('DB_HOST', $_ENV['DB_HOST']);
-define('DB_NAME', $_ENV['DB_NAME']);
-define('DB_USER', $_ENV['DB_USER']);
-define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
 
 class Database
