07528d4cfa1a0dfdde9070a21ac8652444d92e15
Fabio Tedesco <fabio.tedesco1997@gmail.com>
2025-01-17 18:17:23 +0100

    deploy test n.5

--- BE/core/db.php
+++ BE/core/db.php
@@ -7,7 +7,7 @@ require __DIR__ . '/../' . 'vendor/autoload.php';
 define('DB_HOST', getenv('DB_HOST'));
 define('DB_NAME', getenv('DB_NAME'));
 define('DB_USER', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_PASSWORD', getenv('DB_PASSWORD') ?: '');
 
 
 class Database
