39e9cc5807ce4631cbff4e16395c2ef0db4ba498
Fabio Tedesco <fabio.tedesco1997@gmail.com>
2025-01-18 09:17:00 +0100

    deploy test n.6

--- BE/core/db.php
+++ BE/core/db.php
@@ -4,16 +4,18 @@ require __DIR__ . '/../' . 'vendor/autoload.php';
 
 
 
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_NAME', getenv('DB_NAME'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASSWORD') ?: '');
+define('DB_HOST', getenv('MYSQLHOST'));
+define('DB_PORT', getenv('MYSQLPORT'));
+define('DB_NAME', getenv('MYSQLDATABASE'));
+define('DB_USER', getenv('MYSQLUSER'));
+define('DB_PASSWORD', getenv('MYSQLPASSWORD') ?: '');
 
 
 class Database
 {
     // DB params
     private $host = DB_HOST;
+    private $port = DB_PORT;
     private $db_name = DB_NAME;
     private $username = DB_USER;
     private $password = DB_PASSWORD;
@@ -26,7 +28,11 @@ class Database
         $this->conn = null;
 
         try {
-            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO(
+                "pgsql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo 'Connection Error:' . $e->getMessage();
