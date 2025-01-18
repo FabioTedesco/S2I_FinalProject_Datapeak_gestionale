ce366d386647b83f2303026402314f9cbe11f9df
Fabio Tedesco <fabio.tedesco1997@gmail.com>
2025-01-18 11:54:35 +0100

    deploy test n.7.6.1

--- FE/src/pages/Login.jsx
+++ FE/src/pages/Login.jsx
@@ -46,7 +46,8 @@ const Login = () => {
 
       navigate("/");
     } catch (error) {
-      Swal.fire("Errore", `${error.response.data.message}`, "error");
+      console.log(error);
+      Swal.fire("Errore", "Credenziali non valide. Riprova.", "error");
     }
   };
 
